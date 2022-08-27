4a5d967e67ba597de3eb2b57ab1211e83a6b482f
Sonal.J <90455409+Sonaljothylingam@users.noreply.github.com>
2022-08-27 22:41:17 +0530

    Create random-password.py

---
+++ random-password.py
@@ -0,0 +1,9 @@
+ import random
+ upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ lower="abcdefghijklmnopqrstuvwxyz"
+ numbers="0123456789"
+ symbols="[]{}()*;/,._-"
+ all=lower+upper+symbols
+ length=16
+ password="".join(random.sample(all,length))
+ print(password)
